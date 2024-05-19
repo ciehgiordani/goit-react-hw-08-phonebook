00db85df5def777ae96b254c3ed5a8239ba5ec70
ciehgiordani <acieh_giordani@yahoo.com>
2024-05-19 17:39:05 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter basename="/goit-react-hw-08-phonebook-">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
